4c44b4f7bd1d5788bcb09fbac227c3d16561cff8
iqzar <50322762+iqzar@users.noreply.github.com>
2020-06-27 04:27:35 -0700

    Assignment-7

---
+++ app.js
@@ -0,0 +1,18 @@
+function getnum(num){
+    
+    var result=document.getElementById("result");
+    result.value+=num;
+
+}
+function clearresult(){
+    var result=document.getElementById("result");
+    result.value="";
+}
+function getresult(){
+    var result=document.getElementById("result");
+    result.value=eval(result.value);
+}
+function back() {
+    var value = document.getElementById("result").value;
+    document.getElementById("result").value = value.substr(0, value.length - 1);
+}
